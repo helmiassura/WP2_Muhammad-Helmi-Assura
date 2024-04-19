71d776f6c398eafbf62fa6d51fe182b738770513
helmiassura <lerkud600@gmail.com>
2024-04-19 20:04:02 +0700

    menambahkan tugas 5

---
+++ application/views/v_header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8">
+ <title>Web Prog II | Merancang Template sederhana dengan 
+codeigniter</title>
+<link rel="stylesheet" type="text/css" 
+href="http://localhost:8081/helmi/assets/css/stylebuku.css">
+</head>
+<body>
+ <div id=”wrapper”>
+ <header style="background-color: black;">
+ <hgroup>
+ <br>
+ <nav align="right">
+  <ul>
+    <li style="display: inline; margin-right: 10px; color: white;"><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
+    <li style="display: inline; margin-right: 10px;"><a href="<?php echo base_url().'index.php/web/about' ?>">About</a></li>
+  </ul>
+</nav>
+ <h1 style="color: white;">RentalBuku.net</h1>
+ <h3 style="color: white;">Membuat Template Sederhana dengan 
+CodeIgniter</h3>
+<br>
+ </hgroup>
+
+ <div class="clear"></div>
+ </header>
